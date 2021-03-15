a6e67d3a1c9a1129a65d8042c164610a6c3dd6b5
rohandwagh <rohan.wagh@asychron.com>
2021-03-15 12:26:26 +0530

    dashboard

--- asychron-hr-ui/src/App.js
+++ asychron-hr-ui/src/App.js
@@ -4,9 +4,6 @@ import Dashboard from './modules/Dashboard/News/News';
 import Cards from './modules/Dashboard/card/card';
 import Progress from './modules/Dashboard/circularProgress/circularProgress';
 
- 
-//import ApexChart from './modules/Dashboard/circularProgress/circularProgress';
-
 function App() {
   return (
     <div>
